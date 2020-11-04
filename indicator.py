e1ef0e015c5b264288cf998be0ac83ec7fd6a1fd
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2020-11-04 17:09:21 +0700

    MACD

--- indicator.py
+++ indicator.py
@@ -4,9 +4,11 @@ import numpy as np
 
 class Indicator:
 
-    def __init__(self, dataset: DataFrame, rsi_n_day: int = 14):
+    def __init__(self, dataset: DataFrame, rsi_n_day: int = 14, ema_n_day: int = 5, sma_n_day: int = 5):
         self.__dataset = dataset.copy()
         self.__rsi_n_day = rsi_n_day
+        self.__ema_n_day = ema_n_day
+        self.__sma_n_day = sma_n_day
 
     def __rsi_avg_gain(self, dataframe: DataFrame) -> float:
         previous_rs = dataframe.iloc[[-1]]['rs'].values[0]
@@ -61,21 +63,44 @@ class Indicator:
         self.__dataset['rsi'] = 100 - (100 / (1 + self.__dataset['rs']))
         return self.__dataset
 
-    def __SMA(self,EMA_n_day:int) -> float:
-        SMA=(self.__dataset.loc[:EMA_n_day-1,['Close']].sum().values[0])/EMA_n_day
+    def SMA(self, sma_n_day: int = None ) -> float:
+        self.__sma_n_day = sma_n_day if sma_n_day != None else self.__sma_n_day
+        SMA=(self.__dataset.loc[:self.__sma_n_day-1]['Close'].sum())/self.__sma_n_day
         return SMA
 
-    def EMA(self,EMA_n_day) -> DataFrame:
-        self.__dataset['EMA'] = np.nan
-        self.__dataset.loc[EMA_n_day-1,['EMA']] = self.__SMA(EMA_n_day)
+    def EMA(self,EMA_n_day:int = None) -> DataFrame:
+        self.__ema_n_day = EMA_n_day if EMA_n_day != None else self.__ema_n_day
+        column_name = 'ema_{}_day'.format(self.__ema_n_day)
+        self.__dataset[column_name] = np.nan
+        self.__dataset.loc[self.__ema_n_day-1,[column_name]] = self.SMA(self.__ema_n_day)
 
         for index in self.__dataset.index: 
-            if index > (EMA_n_day-1):
-                previous_EMA = self.__dataset.iloc[[index-1]]['EMA'].values[0]
-                EMA = previous_EMA + (2/EMA_n_day+1)*(self.__dataset.iloc[[index]]['Close'].values[0]-previous_EMA)
-                self.__dataset.loc[index,['EMA']] = EMA
-
+            if index > (self.__ema_n_day-1):
+                previous_EMA = self.__dataset.iloc[[index-1]][column_name].values[0]
+                EMA = previous_EMA + (2/self.__ema_n_day+1)*(self.__dataset.iloc[[index]]['Close'].values[0]-previous_EMA)
+                self.__dataset.loc[index,[column_name]] = EMA
         return self.__dataset
-
-    def MACD(self) -> str:
-        return "MACD"
+    
+    
+    def MACD(self, fast_period: int = 12, slow_period: int = 26, signal_period: int = 9) -> DataFrame:
+        self.EMA(fast_period)
+        self.EMA(slow_period)
+        self.__dataset['MACD'] = np.nan
+        self.__dataset['signal'] = np.nan
+        column_slow_period ='ema_{}_day'.format(slow_period)
+        column_fast_period ='ema_{}_day'.format(fast_period)
+        self.__dataset['MACD'] = self.__dataset[column_slow_period] - self.__dataset[column_fast_period]
+        
+        first_ema = self.__dataset.loc[slow_period-1:(slow_period-1)+(signal_period-1)]['MACD'].sum()/signal_period
+        self.__dataset.loc[(slow_period-1)+(signal_period-1),'signal'] = first_ema
+        
+        for index in self.__dataset.index: 
+            if index > (slow_period-1)+(signal_period-1):
+                previous_EMA = self.__dataset.iloc[[index-1]]['signal'].values[0]
+                EMA = previous_EMA + (2/signal_period+1)*(self.__dataset.iloc[[index]]['MACD'].values[0]-previous_EMA)
+                self.__dataset.loc[index,['signal']] = EMA
+                
+        self.__dataset['Histogram'] = self.__dataset['MACD']-self.__dataset['signal']
+        
+        
+        return self.__dataset
