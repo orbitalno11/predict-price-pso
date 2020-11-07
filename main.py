29aefb915fcb0b3b89d1236f8fe0465b5bd315aa
famesensor <famfew25@gmail.com>
2020-11-07 18:58:57 +0700

    [update]: flow program

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from indicator import Indicator
+# from indicator import Indicator
+from preperation import Preperation
+from model import LSTMNN
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 dataset = pd.read_csv('data/AAPL-2014-2018.csv')
-indicator = Indicator(dataset=dataset, rsi_n_day=14)
-rsi_data = indicator.RSI()
+preperation = Preperation(df=dataset)
+data = preperation.calculate_per_change()
 
-sns.lineplot(data=rsi_data[:30], x='Date', y='rsi')
-plt.show()
+model = LSTMNN(epochs=10, batch=10, n_in=30, n_out=3)
+dataset = model.preprocess_data(data, data.columns)
+train_X, train_y, test_X, test_y = model.spilt_data_scale_tranformt(dataset)
+modelLSTM = model.baseline_model(train_X, train_y)
