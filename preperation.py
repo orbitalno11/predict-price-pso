376d942b6cac0bb24415560dcc2fd36a4587114b
famesensor <famfew25@gmail.com>
2020-11-07 19:02:21 +0700

    [update]: add drop date and close

--- preperation.py
+++ preperation.py
@@ -6,8 +6,10 @@ class Preperation:
         self.df = df
 
     def calculate_per_change(self) -> pd.DataFrame:
+        self.df = self.df.drop('Date', axis=1)
         self.df['change'] = (
             (self.df.Close - self.df.Close.shift(1))/self.df.Close.shift(1))*100
         mdf = self.df.drop(0, axis=0)
         mdf = mdf.reset_index(drop=True)
+        mdf = mdf.drop('Close', axis=1)
         return mdf
