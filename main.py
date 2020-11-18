a8beb43c3bf710f2452ec5eb76b022ef93cbb5f4
orbitalno11 <sitthinonit@gmail.com>
2020-11-18 09:55:55 +0700

    [chore]: refactor some part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from indicator import Indicator
-from preperation import Preperation
+from preparation import Preparation
 from model import LSTMNN
 
 import pandas as pd
@@ -9,29 +9,29 @@ import numpy as np
 
 # import data train | stock apple
 dataset = pd.read_csv('data/AAPL-2014-2018.csv')
-# preperation train data
-preperation = Preperation(df=dataset)
-data = preperation.calculate_per_change()
+# preparation train data
+preparation = Preparation(df=dataset)
+data = preparation.calculate_per_change()
 n_in = 30
 n_out = 10
 model = LSTMNN(epochs=10, batch=10, n_in=n_in, n_out=n_out)
 dataset = model.preprocess_data(data, data.columns)
-# split and scale tranform training data
+# split and scale transform training data
 train_X, train_y, sc = model.spilt_data_scale_tranformt(dataset)
 # train model LSTM
 modelLSTM, history = model.baseline_model(train_X, train_y)
 
 # import data test | stock apple
-test_data = pd.read_csv('data/AAPL-30.csv')
-# preperation test data
-preperation_test = Preperation(df=test_data)
+test_data = pd.read_csv('data/AAPLv.2.csv')
+# preparation test data
+preparation_test = Preparation(df=test_data)
 # get close last day
-last_close = preperation_test.getclose()
-data_test = preperation_test.calculate_per_change()
-# split and scale tranform test data
+last_close = preparation_test.get_close()
+data_test = preparation_test.calculate_per_change()
+# split and scale transform test data
 test_sample = model.preprocess_data(data_test.values, data_test.columns)
-test_tranform = sc.transform(test_sample)
-test_X, test_y = test_tranform[:, :-n_out], test_tranform[:, -n_out:]
+test_transform = sc.transform(test_sample)
+test_X, test_y = test_transform[:, :-n_out], test_transform[:, -n_out:]
 test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
 # predict
 predict = modelLSTM.predict(test_X)
@@ -44,7 +44,7 @@ reverse = sc.inverse_transform(temp)
 predict_change = reverse[:, -n_out:]
 
 # change percent change to close
-close_change = preperation_test.getcloseall(predict_change,last_close)
+close_change = preparation_test.get_close_all(predict_change, last_close)
 
 # plt.plot(history.history['loss'], label='loss')
 # plt.plot(history.history['val_loss'], label='val_loss')
@@ -57,16 +57,15 @@ plt.plot(predict_change[0], label='predict_change')
 plt.legend()
 plt.show()
 
+# get Close
+test_close_columns = test_data['Close']
+close_change_columns = close_change['Close']
 
-# get Close 
-test_close_columns=test_data['Close']
-close_change_columns=close_change['Close']
-
-#create close price dataframe 
+# create close price dataframe
 close_columns_frame = [test_close_columns, close_change_columns]
 close_columns = pd.concat(close_columns_frame)
-close_columns=close_columns.reset_index()
-close_columns.drop('index',axis=1,inplace=True)
+close_columns = close_columns.reset_index()
+close_columns.drop('index', axis=1, inplace=True)
 
 indicator = Indicator(close_columns)
 indicator_data = indicator.RSI()
