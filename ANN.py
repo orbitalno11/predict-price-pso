6c665f009c0e50f421ec8cc5b971828af842bfbb
famesensor <famfew25@gmail.com>
2020-12-04 14:35:50 +0700

    [update]: Change Ann

--- ANN.py
+++ ANN.py
@@ -16,7 +16,7 @@ class ANN:
         self.test = dict()
         self.base_line_model = Sequential()
         self.pso_model = Sequential()
-        self.dim = n_in * 6
+        self.dim = 5
 
     def set_train(self, train_x: pd.DataFrame, train_y: pd.DataFrame):
         self.train['x'] = train_x.copy()
@@ -26,49 +26,50 @@ class ANN:
         self.test['x'] = test_x
         self.test['y'] = test_y
 
-    def pre_process_data(self, df: pd.DataFrame, feature_name: list, drop_nan=True) -> np.ndarray:
-        n_col = 1 if type(df) is list else df.shape[1]
-        dataset = pd.DataFrame(df)
-        cols, names, d_col = list(), list(), list()
+    # def pre_process_data(self, df: pd.DataFrame, feature_name: list, drop_nan=True) -> np.ndarray:
+    #     n_col = 1 if type(df) is list else df.shape[1]
+    #     dataset = pd.DataFrame(df)
+    #     cols, names, d_col = list(), list(), list()
 
-        # input sequence (t-n, ... t-1)
-        for i in range(self.n_in, 0, -1):
-            cols.append(dataset.shift(i))
-            names += [f'{feature_name[j]}(t-{i})' for j in range(n_col)]
+    #     # input sequence (t-n, ... t-1)
+    #     for i in range(self.n_in, 0, -1):
+    #         cols.append(dataset.shift(i))
+    #         names += [f'{feature_name[j]}(t-{i})' for j in range(n_col)]
 
-        # forecast sequence (t, t+1, ... t+n)
-        for i in range(0, self.n_out):
-            cols.append(dataset.shift(-i))
-            if i == 0:
-                temp = [f'{feature_name[j]}(t)' for j in range(n_col)]
-                names += temp
-                d_col += temp
-            else:
-                temp = [f'{feature_name[j]}(t+{i})' for j in range(n_col)]
-                names += temp
-                d_col += temp
-        d_col = [val for i, val in enumerate(
-            d_col) if not val.startswith('change')]
-        # put it all together
-        agg = pd.concat(cols, axis=1)
-        agg.columns = names
-        if drop_nan:
-            agg.dropna(inplace=True)
-        agg.drop(d_col, axis=1, inplace=True)
-        return agg
+    #     # forecast sequence (t, t+1, ... t+n)
+    #     for i in range(0, self.n_out):
+    #         cols.append(dataset.shift(-i))
+    #         if i == 0:
+    #             temp = [f'{feature_name[j]}(t)' for j in range(n_col)]
+    #             names += temp
+    #             d_col += temp
+    #         else:
+    #             temp = [f'{feature_name[j]}(t+{i})' for j in range(n_col)]
+    #             names += temp
+    #             d_col += temp
+    #     d_col = [val for i, val in enumerate(
+    #         d_col) if not val.startswith('change')]
+    #     # put it all together
+    #     agg = pd.concat(cols, axis=1)
+    #     agg.columns = names
+    #     if drop_nan:
+    #         agg.dropna(inplace=True)
+    #     agg.drop(d_col, axis=1, inplace=True)
+    #     return agg
 
     def split_data_scale_transform(self, df: pd.DataFrame) -> tuple:
         # Feature Scaling
         df = df.to_numpy()
+        df_x , df_y = df[:,:-self.n_out], df[:,-self.n_out:]
         sc = MinMaxScaler(feature_range=(0, 1))
         training_set_scaled = sc.fit_transform(df)
         train_X, train_y = training_set_scaled[:, :-self.n_out], training_set_scaled[:, -self.n_out:]
-        return train_X, train_y, sc
+        return train_X, train_y , sc, df_x, df_y
 
     def initial_baseline(self):
         self.base_line_model = Sequential()
-        self.base_line_model.add(Dense(self.n_in, input_dim=self.dim, activation='relu'))
-        self.base_line_model.add(Dense(self.n_in * 2, activation='relu'))
+        self.base_line_model.add(Dense(self.dim, input_dim=self.dim, activation='relu'))
+        self.base_line_model.add(Dense(self.dim * 2, activation='relu'))
         self.base_line_model.add(Dense(self.n_out, activation='sigmoid'))
         self.base_line_model.compile(optimizer='adam', loss='mean_squared_error')
 
