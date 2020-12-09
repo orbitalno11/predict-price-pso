f89a892942e315831db4e7278d9e05843ffd05d6
famesensor <famfew25@gmail.com>
2020-12-09 15:18:25 +0700

    [update]: update feature

--- main2.py
+++ main2.py
@@ -8,8 +8,8 @@ from ANN import ANN
 from indicator import Indicator
 
 # initial value
-TRAIN_PATH = 'data/test_set/C-18.csv'
-PATH_ANN_MODEL = 'model/model-baseline-new/model-c18/c'
+TRAIN_PATH = 'data/test_set/WMT-20.csv'
+PATH_ANN_MODEL = 'model/model-baseline-new/model-wmt20'
 # PATH_PSO_MODEL = 'model/model-pso-new/model-pso-c'
 
 N_IN = 5  # number of date for training
@@ -37,15 +37,14 @@ indicator_data = indicator.MACD()
 indicator_data.dropna(inplace=True)
 
 indicator_data['Change of EMA'] = (
-    (indicator_data['ema_5_day'] - indicator_data['Close']) / indicator_data['ema_5_day']) * 100
+    (indicator_data['Close'] - indicator_data['ema_5_day']) / indicator_data['ema_5_day']) * 100
 data_set = indicator_data[['rsi', 'Histogram', 'Change of EMA', 'change']]
 ann.split_data_scale_transform(data_set)
 
 # training baseline model
 history = ann.baseline_train()
 annModel = ann.get_baseline_model()
-# annModel.save(PATH_ANN_MODEL)
-annModel.summary()
+annModel.save(PATH_ANN_MODEL)
 
 # # test baseline model
 # ann.baseline_test()
@@ -79,67 +78,3 @@ annModel.summary()
 # pso_model = ann.evaluate_model_pso(pos)
 # pso_predict = pso_model.predict(test_X)
 # pso_model.save(PATH_PSO_MODEL)
-
-# # # reversed value to original value
-# # pso_temps = np.concatenate((test_X[0].reshape(1, DIM), pso_predict), axis=1)
-# # pso_reversed = sc.inverse_transform(pso_temps)
-# # pso_predict_changes = pso_reversed[:, -N_OUT:]
-
-# # ann_predict_temps = np.concatenate((test_X[0].reshape(1, DIM), predict), axis=1)
-# # ann_predict_reversed = sc.inverse_transform(ann_predict_temps)
-# # ann_predict_changes = ann_predict_reversed[:, -N_OUT:]
-
-# # # transform percent change to close value
-# # close_change_ann = preparation_test.get_close_all(ann_predict_changes, last_close)
-# # close_change_pso = preparation_test.get_close_all(pso_predict_changes, last_close)
-
-# # # get close value
-# # test_close_columns = test_data['Close']
-# # close_change_columns_ann = close_change_ann['Close']
-# # close_change_columns_pso = close_change_pso['Close']
-
-# # # create close price dataframe
-# # close_columns_frame_ann = [test_close_columns[:-N_OUT], close_change_columns_ann]
-# # close_columns_ann = pd.concat(close_columns_frame_ann)
-# # close_columns_ann = close_columns_ann.reset_index()
-# # close_columns_ann.drop('index', axis=1, inplace=True)
-
-# # close_columns_frame_pso = [test_close_columns[:-N_OUT], close_change_columns_pso]
-# # close_columns_pso = pd.concat(close_columns_frame_pso)
-# # close_columns_pso = close_columns_pso.reset_index()
-# # close_columns_pso.drop('index', axis=1, inplace=True)
-
-# # # create indicator
-# # indicator_ann_predict = Indicator(close_columns_ann)
-# # indicator_data_ann_predict = indicator_ann_predict.RSI()
-# # indicator_data_ann_predict = indicator_ann_predict.EMA()
-# # indicator_data_ann_predict = indicator_ann_predict.MACD()
-
-# # indicator_pso_predict = Indicator(close_columns_pso)
-# # indicator_data_pso_predict = indicator_pso_predict.RSI()
-# # indicator_data_pso_predict = indicator_pso_predict.EMA()
-# # indicator_data_pso_predict = indicator_pso_predict.MACD()
-
-# # indicator_test = Indicator(test_data)
-# # indicator_test_data = indicator_test.RSI()
-# # indicator_test_data = indicator_test.EMA()
-# # indicator_test_data = indicator_test.MACD()
-
-# # show loss
-# # plt.plot(history.history['loss'], label='ann_loss')
-# # plt.plot(pso_model.history['loss'], label='pso_loss')
-# # plt.title('Mean Square Error')
-# # plt.ylabel('error')
-# # plt.xlabel('epoch')
-# # plt.legend(loc='upper left')
-# # plt.show()
-
-# # # show close value
-# # plt.plot(predict, label='ANN')
-# # plt.plot(pso_predict, label='PSO')
-# # plt.plot(test_y, label='REAL VALUE')
-# # plt.title('Close')
-# # plt.ylabel('Close')
-# # plt.xlabel('number of date')
-# # plt.legend(loc='upper left')
-# # plt.show()
